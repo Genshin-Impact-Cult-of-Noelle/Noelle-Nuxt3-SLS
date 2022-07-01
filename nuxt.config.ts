ddad0319e271e790be9a9b29d3f31b7237c773d1
YueAo7 <leaoqi@outlook.com>
2022-07-01 20:21:52 +0800

    ✨ feat(nuxt配置): 资源文件

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,21 +1,47 @@
+/*
+ * @Date: 2022-07-01 13:15:06
+ * @LastEditors: YueAo7
+ * @LastEditTime: 2022-07-01 19:54:20
+ * @FilePath: \Noelle-Nuxt3-SLS\nuxt.config.ts
+ */
 import { defineNuxtConfig } from "nuxt";
-
+const isProd = process.env.NODE_ENV === "production";
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  env: {
+    STATIC_URL: isProd ? `https://${process.env.STATIC_URL}` : "",
+  },
+  app:{
+buildAssetsDir:"/love/"
+  },
+  alias:{
+  },
   build: {
+    // analyze:true,
     transpile: [
       "naive-ui",
       "vueuc",
       "@css-render/vue3-ssr",
       "@juggle/resize-observer",
     ],
+    publicPath:process.env.STATIC_URL,
   },
   css:[
     "@/assets/css/public/index.less"
   ],
+  
   vite: {
     optimizeDeps: {
       include: ["date-fns-tz/esm/formatInTimeZone"],
+      
     },
+    build:{
+      rollupOptions:{
+        output:{
+          assetFileNames:`at/lbrary/[name].[ext]`
+        }
+      }
+    }
+    
   },
 });

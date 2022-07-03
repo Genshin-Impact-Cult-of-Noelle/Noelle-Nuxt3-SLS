44a89658f68a4249e8010315ac3937ec70885981
YueAo7 <leaoqi@outlook.com>
2022-07-03 21:36:54 +0800

    ✨ feat(首页): 完成第一栏

--- pages/index.vue
+++ pages/index.vue
@@ -1,48 +1,62 @@
 <!--
  * @Date: 2022-07-01 13:15:06
  * @LastEditors: YueAo7
- * @LastEditTime: 2022-07-01 18:50:43
+ * @LastEditTime: 2022-07-03 21:36:45
  * @FilePath: \Noelle-Nuxt3-SLS\pages\index.vue
 -->
 <template>
   <NSpace>
-    <NCarousel  direction="vertical" style="width: 100vw; height: 100vh" mousewheel :loop="false" draggable>
+    <NCarousel
+      direction="vertical"
+      style="width: 100vw; height: 100vh"
+      mousewheel
+      :loop="false"
+      draggable
+      :show-dots="false"
+      @update:currentIndex="onCurrentPageChange"
+    >
+      <HomePageFirst :isShow="curPageIndex == 0"> </HomePageFirst>
+      <HomePageSecond :isShow="curPageIndex == 0"> </HomePageSecond>
 
-      <div class="carousel-img head-1">
-        <img class="bg" src="@/assets/image/test/t.jpg" alt="" srcset="">
-      </div>
-      <div class="carousel-img head-1">
-        <img class="bg" src="@/assets/image/test/t.jpg" alt="" srcset="">
-      </div>
-      <div class="carousel-img head-1">
-        <img class="bg" src="@/assets/image/test/t.jpg" alt="" srcset="">
+      <!-- <div class="carousel-img head-1" :class="{ now: curPageIndex == 0 }">
+        <img class="bg" src="@/assets/image/test/t.jpg" alt="" srcset="" />
+        <div class="content">
+          <div class="box">
+            <div class="title">BARBARA CHURCH</div>
+            <div class="text">艾伯特拯救世界</div>
+            <div class="button">Get Started</div>
+          </div>
+        </div>
+      </div> -->
+
+      <div class="carousel-img head-1" :class="{ now: curPageIndex == 2 }">
+        <img class="bg" src="@/assets/image/test/t.jpg" alt="" srcset="" />
       </div>
       <!-- <div v-for="(i, v) in [5, 4, 3, 2, 1]" class="carousel-img">      </div> -->
-
     </NCarousel>
   </NSpace>
 </template>
 
 <script lang="ts" setup>
-import {
-  NSpace,
-  NCarousel,
-} from "naive-ui";
-
-
+import { NSpace, NCarousel, NButton, NTabs, NTabPane } from "naive-ui";
+const curPageIndex = ref(0);
+const curPageIndex2 = ref(0);
+const onCurrentPageChange = (curIndex: number, lastIndex: number) => {
+  curPageIndex.value = curIndex;
+};
+useHead({
+  title: "芭芭拉神教",
+  viewport: "width=device-width, initial-scale=1, maximum-scale=1",
+  charset: "utf-8",
+});
 </script>
 
 <style lang="less" scoped>
+@import "@/assets/css/public/index.less";
 
-.carousel-img{
+.carousel-img {
   width: 100vw;
   height: 100vh;
-  //1280*720
 }
-.head-1 {
-  .bg {
-    width: 100%;
-    height: 100%;
-  }
-}
+
 </style>
